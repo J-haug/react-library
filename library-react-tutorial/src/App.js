aa98fa5f89206f196dc5a37664da512ab8bc7b66
J-haug <jeremiahhaugan@gmail.com>
2025-12-10 11:28:16 -0600

    1

--- library-react-tutorial/src/App.js
+++ library-react-tutorial/src/App.js
@@ -5,20 +5,24 @@ import Footer from "./components/Footer";
 import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home /> }/>
+          <Route path="/books" element={<Books books={books}/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
- 
+
 export default App;
